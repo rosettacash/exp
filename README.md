83cdca17cf33e9d5d0fea37990f381a8ecb69344
LJZN <rubyminer@foxmail.com>
2018-09-19 03:25:26 +0800

    simple input

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # exp
+
+- input: BitcoinCash transaction hash
+- output: transaction info
+
+## todo
+
+- [ ] input box
+- [ ] blockchair api
+- [ ] response box
+- [ ] bitcoin.com api
+- [ ] blocktrail api
+- [ ] btc.com api
+- [ ] time record
--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,9 @@
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
 import Browser
+import Json.Decode as Decode
 
 main : Program () Model Msg
 main =
@@ -13,28 +17,64 @@ main =
 
 type alias Model =
     { input : String
+    , response : String
     }
 
 
 type Msg
-    = Msg1
-    | Msg2
+    = Validate String
+    | TxInfo (Result Http.Error String)
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
-        Msg1 ->
-            (model, Cmd.none)
+        TxInfo result ->
+          case result of
+            Ok info ->
+              ( { model | response = info }
+              , Cmd.none
+              )
 
-        Msg2 ->
-            (model, Cmd.none)
+            Err _ ->
+              ( { model | response = "Error" }
+              , Cmd.none
+              )
 
+        Validate txHash ->
+            if String.length txHash == 64 then
+              ( { model
+                  | response = "Requesting..."
+                }
+              , getTxInfo txHash
+              )
+            else
+              ( model, Cmd.none )
+
+
+getTxInfo : String -> Cmd Msg
+getTxInfo txHash =
+  Http.send TxInfo (Http.getString (blockChairApi txHash))
+
+
+blockChairApi : String -> String
+blockChairApi txHash =
+  "https://api.blockchair.com/bitcoin-cash/transactions?q=hash(" ++ txHash ++ ")"
+
+
+-- blockChairDecoder : Decode.Decoder String
+-- blockChairDecoder =
+--   Decode.string
 
 view : Model -> Html Msg
 view model =
     div []
-        [ text "New Html Program" ]
+    [ input
+      [ onInput Validate
+      ]
+      []
+    , div [] [ text model.response ]
+    ]
 
 
 subscriptions : Model -> Sub Msg
@@ -42,8 +82,8 @@ subscriptions model =
     Sub.none
 
 
-init : () -> (Model, Cmd Msg)
+init : () -> ( Model, Cmd Msg)
 init _ =
-    ( Model "Hello"
+    ( Model "" "Please enter the transaction hash."
     , Cmd.none
     )
